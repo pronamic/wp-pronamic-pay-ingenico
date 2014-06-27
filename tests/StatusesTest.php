05bf6c5b657a26202b3d5f1dec246bec5f88cce7
Remco Tolsma <info@remcotolsma.nl>
2014-06-27 08:50:43 +0200

    Added tests for all statuses.

--- tests/StatusesTest.php
+++ tests/StatusesTest.php
@@ -36,6 +36,33 @@ class Pronamic_WP_Pay_Gateways_Ogone_StatusesTest extends PHPUnit_Framework_Test
 			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHORIZED_AND_CANCELLED_64, Pronamic_WP_Pay_Statuses::CANCELLED ),
 			// Open
 			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::ORDER_STORED, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::STORED_WAITING_EXTERNAL_RESULT, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::WAITING_CLIENT_PAYMENT, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUHTORIZED_WAITING_EXTERNAL_RESULT, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHORIZATION_WAITING, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHORIZATION_NOT_KNOWN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::STAND_BY, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::OK_WITH_SCHEDULED_PAYMENTS, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::ERROR_IN_SCHEDULED_PAYMENTS, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUHORIZ_TO_GET_MANUALLY, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHOR_DELETION_WAITING, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHOR_DELETION_UNCERTAIN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_DELETION_PENDING, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_DELETION_UNCERTAIN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_DELETED_74, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::DELETION_PROCESSED_BY_MERCHANT, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::REFUND_PENDING, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::REFUND_UNCERTAIN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_UNCERTAIN, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_PROCESSING, Pronamic_WP_Pay_Statuses::OPEN ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::BEING_PROCESSED, Pronamic_WP_Pay_Statuses::OPEN ),
+			// Success
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::AUTHORIZED, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_DELETED, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::REFUND, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::REFUND_PROCESSED_BY_MERCHANT, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_REQUESTED, Pronamic_WP_Pay_Statuses::SUCCESS ),
+			array( Pronamic_WP_Pay_Gateways_Ogone_Statuses::PAYMENT_PROCESSED_BY_MERCHANT, Pronamic_WP_Pay_Statuses::SUCCESS ),
 		);
     }
 }
