6f29463acd0ff8ff558d542eb8a353c8edadbf86
Remco Tolsma <info@remcotolsma.nl>
2015-02-18 13:52:18 +0100

    Added test for the Ogone data URL helper class.

---
+++ tests/DataCreditCardHelperTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Title: Ogone data default helper class test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @since 1.1.0
+ */
+class Pronamic_WP_Pay_Gateways_Ogone_DataCreditCardHelperTest extends WP_UnitTestCase {
+	/**
+	 * Test helper
+	 */
+	function test_helper() {
+		$data = new Pronamic_WP_Pay_Gateways_Ogone_Data();
+
+		$helper = new Pronamic_WP_Pay_Gateways_Ogone_DataCreditCardHelper( $data );
+
+		$ed = new DateTime();
+
+		$helper
+			->set_number( '378282246310005' )
+			->set_expiration_date( $ed )
+			->set_security_code( '123' )
+		;
+
+		$this->assertEquals( array(
+			'CARDNO' => '378282246310005',
+			'ED'     => $ed->format( Pronamic_WP_Pay_Gateways_Ogone_Ogone::EXPIRATION_DATE_FORMAT ),
+			'CVC'    => '123',
+		), $data->get_fields() );
+	}
+}
