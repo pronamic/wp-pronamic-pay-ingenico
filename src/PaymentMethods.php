4efa893535ff07c3ffee835bf3ec3ac5105e2c24
Remco Tolsma <info@remcotolsma.nl>
2015-02-18 11:15:49 +0100

    Added an constants class for Ogone payment methods.

---
+++ src/PaymentMethods.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Title: Ogone payment methods (PM parameter)
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.1.0
+ * @see https://github.com/wp-pay-gateways/ogone/wiki/Brands
+ */
+class Pronamic_WP_Pay_Gateways_Ogone_PaymentMethods {
+	/**
+	 * Acceptgiro
+	 *
+	 * @var string
+	 */
+	const ACCEPTGIRO = 'Acceptgiro';
+
+	/**
+	 * Bank transfer
+	 *
+	 * @var string
+	 */
+	const BANK_TRANSFER = 'Bank transfer';
+
+	/**
+	 * Credit Card
+	 *
+	 * @var string
+	 */
+	const CREDIT_CARD = 'CreditCard';
+
+	/**
+	 * DirectEbanking
+	 *
+	 * @var string
+	 */
+	const DIRECT_EBANKING = 'DirectEbanking';
+
+	/**
+	 * DirectEbankingNL
+	 *
+	 * @var string
+	 */
+	const DIRECT_EBANKING_NL = 'DirectEbankingNL';
+
+	/**
+	 * iDEAL
+	 *
+	 * @var string
+	 */
+	const IDEAL = 'iDEAL';
+
+	/**
+	 * Payment on Delivery
+	 *
+	 * @var string
+	 */
+	const PAYMENT_ON_DELIVERY = 'Payment on Delivery';
+
+	/**
+	 * PayPal
+	 *
+	 * @var string
+	 */
+	const PAYPAL = 'PAYPAL';
+
+	/**
+	 * Wallie
+	 *
+	 * @var string
+	 */
+	const WALLIE = 'Wallie';
+}
