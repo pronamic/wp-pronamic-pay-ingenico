b7fac398dd6f75829b2e640a45c1263ed7874b2f
Remco Tolsma <info@remcotolsma.nl>
2015-02-18 11:55:18 +0100

    Added an test for general data helper class.

--- tests/DataGeneralHelperTest.php
+++ tests/DataGeneralHelperTest.php
@@ -17,11 +17,38 @@ class Pronamic_WP_Pay_Gateways_Ogone_DataGeneralHelperTest extends WP_UnitTestCa
 
 		$helper = new Pronamic_WP_Pay_Gateways_Ogone_DataGeneralHelper( $data );
 
+		$pmlist = new Pronamic_WP_Pay_Gateways_Ogone_PaymentMethodsList( array(
+			Pronamic_WP_Pay_Gateways_Ogone_Brands::VISA,
+			Pronamic_WP_Pay_Gateways_Ogone_Brands::MASTERCARD,
+			Pronamic_WP_Pay_Gateways_Ogone_Brands::AMERICAN_EXPRESS,
+		) );
+
 		$helper
 			->set_psp_id( 'test' )
-			->set_order_id( '1234' );
+			->set_order_id( '1234' )
+			->set_order_description( 'order description' )
+			->set_amount( 10.50 )
+			->set_currency( 'EUR' )
+			->set_customer_name( 'Mr. Test' )
+			->set_email( 'test@example.com' )
+			->set_language( 'nl' )
+			->set_payment_method( Pronamic_WP_Pay_Gateways_Ogone_PaymentMethods::IDEAL )
+			->set_payment_methods_list( $pmlist )
+			->set_brand( Pronamic_WP_Pay_Gateways_Ogone_Brands::IDEAL )
+		;
 
-		$this->assertEquals( 'test', $data->get_field( 'PSPID' ) );
-		$this->assertEquals( '1234', $data->get_field( 'ORDERID' ) );
+		$this->assertEquals( array(
+			'PSPID'    => 'test',
+			'ORDERID'  => '1234',
+			'COM'      => 'order description',
+			'AMOUNT'   => 1050,
+			'CURRENCY' => 'EUR',
+			'CN'       => 'Mr. Test',
+			'EMAIL'    => 'test@example.com',
+			'LANGUAGE' => 'nl',
+			'PM'       => Pronamic_WP_Pay_Gateways_Ogone_PaymentMethods::IDEAL,
+			'PMLIST'   => (string) $pmlist,
+			'BRAND'    => Pronamic_WP_Pay_Gateways_Ogone_Brands::IDEAL,
+		), $data->get_fields() );
 	}
 }
