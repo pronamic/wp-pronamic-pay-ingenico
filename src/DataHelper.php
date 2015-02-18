d706611587fe7022e2de75adfd68ba10569c0c65
Remco Tolsma <info@remcotolsma.nl>
2015-02-18 11:39:35 +0100

    Added an abstract Ogone data helper class.

---
+++ src/DataHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Title: Ogone data helper class
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @since 1.1.0
+ */
+abstract class Pronamic_WP_Pay_Gateways_Ogone_DataHelper {
+	/**
+	 * Data
+	 *
+	 * @var array
+	 */
+	protected $data;
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initialize a Ogone data default helper class
+	 */
+	public function __construct( Pronamic_WP_Pay_Gateways_Ogone_Data $data ) {
+		$this->data = $data;
+	}
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Set field
+	 *
+	 * @param string $name
+	 * @param string $value
+	 */
+	public function set_field( $name, $value ) {
+		$this->data->set_field( $name, $value );
+
+		return $this;
+	}
+}
