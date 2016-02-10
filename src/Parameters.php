6caaa7a7fcaac0142d90223e8eba2ffa17d4db28
Remco Tolsma <info@remcotolsma.nl>
2016-02-10 09:04:15 +0100

    Happy 2016.

--- src/Parameters.php
+++ src/Parameters.php
@@ -3,7 +3,7 @@
 /**
  * Title: Ogone parameters constants
  * Description:
- * Copyright: Copyright (c) 2005 - 2015
+ * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  *
  * @author Remco Tolsma
