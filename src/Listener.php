5ed78e43a3eca62123d0733bc26ad2e3f9ca49b6
Remco Tolsma <info@remcotolsma.nl>
2016-02-10 10:09:53 +0100

    Fixed indent.

--- src/Listener.php
+++ src/Listener.php
@@ -2,13 +2,14 @@
 
 /**
  * Title: OmniKassa listener
-* Description:
-* Copyright: Copyright (c) 2005 - 2016
-* Company: Pronamic
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
  *
-* @author Remco Tolsma
-* @version 1.0.0
-*/
+ * @author Remco Tolsma
+ * @version 1.2.6
+ * @since 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Ogone_Listener implements Pronamic_Pay_Gateways_ListenerInterface {
 	public static function listen() {
 		$data = Pronamic_WP_Pay_Gateways_Ogone_Security::get_request_data();
