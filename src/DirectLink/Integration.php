2a61c00c2ae9c985f7141ab3a7454388215b95a6
Remco Tolsma <info@remcotolsma.nl>
2016-02-18 12:38:48 +0100

    Added support for get_settings().

--- src/DirectLink/Integration.php
+++ src/DirectLink/Integration.php
@@ -19,4 +19,19 @@ class Pronamic_WP_Pay_Gateways_Ogone_DirectLink_Integration extends Pronamic_WP_
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_Ogone_DirectLink_Gateway';
 	}
+
+	/**
+	 * Get required settings for this integration.
+	 *
+	 * @see https://github.com/wp-premium/gravityforms/blob/1.9.16/includes/fields/class-gf-field-multiselect.php#L21-L42
+	 * @since 1.1.3
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = parent::get_settings();
+
+		$settings[] = 'ogone_directlink';
+
+		return $settings;
+	}
 }
