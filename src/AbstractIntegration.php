ce67aff4f45788b076f773b6ddd094d154827dff
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 17:10:56 +0100

    Add product and dashboard URLs

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -2,8 +2,10 @@
 
 abstract class Pronamic_WP_Pay_Gateways_Ogone_AbstractIntegration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
-		$this->url      = 'https://secure.ogone.com/';
-		$this->provider = 'ogone';
+		$this->url           = 'https://secure.ogone.com/';
+		$this->product_url   = __( 'https://payment-services.ingenico.com/nl/en', 'pronamic_ideal' );
+		$this->dashboard_url = 'https://secure.ogone.com/';
+		$this->provider      = 'ogone';
 
 		// Actions
 		$function = array( 'Pronamic_WP_Pay_Gateways_Ogone_Listener', 'listen' );
