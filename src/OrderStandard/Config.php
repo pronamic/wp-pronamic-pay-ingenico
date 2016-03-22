968a68fac28f9a5e9a3f7c56d9f2faf6d5ada566
Remco Tolsma <info@remcotolsma.nl>
2016-03-22 16:33:51 +0100

    Getting ready for version 1.2.8.

--- src/OrderStandard/Config.php
+++ src/OrderStandard/Config.php
@@ -5,8 +5,10 @@
  * Description:
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
+ *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.2.8
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config extends Pronamic_WP_Pay_Gateways_Ogone_Config {
 	public $hash_algorithm;
--- src/OrderStandard/TestConfig.php
+++ src/OrderStandard/TestConfig.php
@@ -5,8 +5,10 @@
  * Description:
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
+ *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.2.8
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_TestConfig extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config {
 	public function get_payment_server_url() {
