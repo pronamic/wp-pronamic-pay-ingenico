27783aff1d22ca853fcae41a1cd0e9e5388a6830
Remco Tolsma <info@remcotolsma.nl>
2016-03-31 13:32:55 +0200

    Happy 2016.

--- tests/DataCreditCardHelperTest.php
+++ tests/DataCreditCardHelperTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: Ogone data default helper class test
  * Description:
- * Copyright: Copyright (c) 2005 - 2015
+ * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  *
  * @author Remco Tolsma
