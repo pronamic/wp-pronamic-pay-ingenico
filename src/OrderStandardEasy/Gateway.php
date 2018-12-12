74bb7f779879bd6ef8894f6157e863fac3c91a27
Reüel van der Steege <reuel@pronamic.nl>
2018-12-12 13:49:53 +0100

    Getting ready for version 2.0.1.

--- src/OrderStandardEasy/Gateway.php
+++ src/OrderStandardEasy/Gateway.php
@@ -17,7 +17,7 @@ use Pronamic\WordPress\Pay\Payments\Payment;
  * Company: Pronamic
  *
  * @author  Remco Tolsma
- * @version 2.0.0
+ * @version 2.0.1
  * @since   1.0.0
  */
 class Gateway extends Core_Gateway {
