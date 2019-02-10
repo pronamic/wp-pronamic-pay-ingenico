dda61ff9853a678faf930e9f18b47af3ece3b69e
Reüel van der Steege <reuel@pronamic.nl>
2019-02-10 22:06:00 +0100

    Update Grunt phpcs config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,14 @@ module.exports = function( grunt ) {
 				src: [
 					'**/*.php',
 					'!node_modules/**',
-					'!vendor/**'
+					'!vendor/**',
+					'!wordpress/**',
+					'!wp-content/**'
 				]
 			},
 			options: {
 				bin: 'vendor/bin/phpcs',
-				standard: 'phpcs.ruleset.xml',
+				standard: 'phpcs.xml.dist',
 				showSniffCodes: true
 			}
 		},
