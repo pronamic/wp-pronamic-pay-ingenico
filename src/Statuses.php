35cee1b541da333359469afea89abe96eb71256b
Reüel van der Steege <reuel@pronamic.nl>
2019-09-27 09:35:18 +0200

    Move class Core\Statuses to Payments\PaymentStatus.

--- src/Statuses.php
+++ src/Statuses.php
@@ -2,7 +2,7 @@
 
 namespace Pronamic\WordPress\Pay\Gateways\Ingenico;
 
-use Pronamic\WordPress\Pay\Core\Statuses as Core_Statuses;
+use Pronamic\WordPress\Pay\Payments\PaymentStatus as Core_Statuses;
 
 /**
  * Title: Ingenico statuses constants
