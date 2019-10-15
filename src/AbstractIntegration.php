f1d405eb1c18a262f38145d376852ed9b26a6415
Reüel van der Steege <reuel@akwaabamultimedia.nl>
2019-10-15 22:01:03 +0200

    Add manual URL.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -13,6 +13,7 @@ abstract class AbstractIntegration extends Common_AbstractIntegration {
 				'name'          => null,
 				'url'           => 'https://secure.ogone.com/',
 				'product_url'   => __( 'https://payment-services.ingenico.com/nl/en', 'pronamic_ideal' ),
+				'manual_url'    => __( 'https://www.pronamic.eu/support/how-to-connect-ingenico-with-wordpress-via-pronamic-pay/', 'pronamic_ideal' ),
 				'dashboard_url' => 'https://secure.ogone.com/',
 				'provider'      => 'ogone',
 			)
