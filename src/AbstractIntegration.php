98a1228cb2c4781047c12a2f00865f68b97a9a41
Reüel van der Steege <reuel@pronamic.nl>
2022-11-29 22:07:44 +0100

    Update manual URL to pronamicpay.com (pronamic/pronamic-pay#15).

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -13,7 +13,7 @@ abstract class AbstractIntegration extends AbstractGatewayIntegration {
 				'name'          => null,
 				'url'           => 'https://secure.ogone.com/',
 				'product_url'   => \__( 'https://payment-services.ingenico.com/nl/en', 'pronamic_ideal' ),
-				'manual_url'    => \__( 'https://www.pronamic.eu/support/how-to-connect-ingenico-with-wordpress-via-pronamic-pay/', 'pronamic_ideal' ),
+				'manual_url'    => \__( 'https://www.pronamicpay.com/en/manuals/how-to-connect-ingenico-to-wordpress-with-pronamic-pay/', 'pronamic_ideal' ),
 				'dashboard_url' => 'https://secure.ogone.com/',
 				'provider'      => 'ogone',
 				'supports'      => [
