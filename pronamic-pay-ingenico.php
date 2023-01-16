ebf302c31effe3b911501291ebfeaa3e6d31b2c6
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-01-16 20:29:29 +0100

    Add wp-env.

---
+++ pronamic-pay-ingenico.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay Igenico Add-On
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-igenico/
+ * Description: Extend the Pronamic Pay plugin with the Igenico gateway to receive payments with Igenico through a variety of WordPress plugins.
+ *
+ * Version: 4.3.0
+ * Requires at least: 4.7
+ * Requires PHP: 8.0
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-igenico
+ * Domain Path: /languages/
+ *
+ * License: GPL-3.0-or-later
+ *
+ * Requires Plugins: pronamic-ideal
+ * Depends: wp-pay/core
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-gravityforms
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Gateways\Ingenico
+ */
