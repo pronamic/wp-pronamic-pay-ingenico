e41b724cd751942038a36655177bb3764ed1d053
Reüel van der Steege <reuel@pronamic.nl>
2025-10-01 15:06:11 +0200

    Add this point we shoud not use translation functions.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -17,8 +17,8 @@ abstract class AbstractIntegration extends AbstractGatewayIntegration {
 				'id'            => null,
 				'name'          => null,
 				'url'           => 'https://secure.ogone.com/',
-				'product_url'   => \__( 'https://payment-services.ingenico.com/nl/en', 'pronamic_ideal' ),
-				'manual_url'    => \__( 'https://www.pronamicpay.com/en/manuals/how-to-connect-ingenico-to-wordpress-with-pronamic-pay/', 'pronamic_ideal' ),
+				'product_url'   => 'https://worldline.com/',
+				'manual_url'    => 'https://www.pronamicpay.com/en/manuals/how-to-connect-ingenico-to-wordpress-with-pronamic-pay/',
 				'dashboard_url' => 'https://secure.ogone.com/',
 				'provider'      => 'ogone',
 				'supports'      => [
